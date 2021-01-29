6fe5639278c4fdb34e51350b69751d85a8e7f37f
Jordan-Madden <jordanmadden285@gmail.com>
2021-01-29 10:34:42 -0500

    Created a function that validates a given password

--- lab1.py
+++ lab1.py
@@ -6,10 +6,37 @@
 def hello():
     print("ECSE3038 - Engineering IoT Systems")
 
+def validatePassword(password):
+    alnum, nums = 0, 0
+    
+    if len(password) >= 8:
+        for val in password:
+            if val.isalnum():
+                try:
+                    if isinstance(int(val), int):
+                        nums += 1
+                except:
+                    pass
+            else:
+                print("[ERROR] The password can only have alphanumeric values") 
+                continue
+        if nums >= 2:
+            return True
+        else:
+            print("[ERROR] The password must have more than 2 numbers.")
+    else:
+        print("[ERROR] Password must have at least 8 letters")
+    
+    return False
+
 if __name__ == "__main__":
     # Part 1
     hello()
+    print("\n")
 
-        
+    # Part 2
+    password = "GreetingsFe11owKids"
+    print(validatePassword(password))
+    print("\n")
                 
 
