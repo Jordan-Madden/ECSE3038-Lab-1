f67ce2f8d625dcc3b362399eafe924439f265cd5
Jordan-Madden <jordanmadden285@gmail.com>
2021-01-29 10:32:03 -0500

    First commit. Tackling the 1st part of the lab

---
+++ lab1.py
@@ -0,0 +1,15 @@
+'''
+    Author: Jordan Madden
+    Usage: python lab1.py
+'''
+
+def hello():
+    print("ECSE3038 - Engineering IoT Systems")
+
+if __name__ == "__main__":
+    # Part 1
+    hello()
+
+        
+                
+
